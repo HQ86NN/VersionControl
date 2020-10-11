1b802428044dff33b68b1d23d5f688a9a68a9b4a
Péter Gömöry <mozsunevol@gmail.com>
2020-10-11 18:34:08 +0200

    koordinata

--- Task4/Task4/Form1.cs
+++ Task4/Task4/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.IO;
 
 namespace Task4
 {
@@ -16,15 +17,16 @@ namespace Task4
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
-        Excel.Application xlApp; // A Microsoft Excel alkalmazás
-        Excel.Workbook xlWB; // A létrehozott munkafüzet
-        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+        Excel.Application xlApp;
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             
+
         }
         private void LoadData()
         {
@@ -34,34 +36,77 @@ namespace Task4
         {
             try
             {
-                // Excel elindítása és az applikáció objektum betöltése
                 xlApp = new Excel.Application();
-
-                // Új munkafüzet
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
-
-                // Új munkalap
-                xlSheet = xlWB.ActiveSheet;
-
-                // Tábla létrehozása
-                //CreateTable(); // Ennek megírása a következő feladatrészben következik
-
-                // Control átadása a felhasználónak
+                xlSheet = xlWB.ActiveSheet;                
+                CreateTable(); 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
-            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            catch (Exception ex) 
             {
                 string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(errMsg, "Error");
-
-                // Hiba esetén az Excel applikáció bezárása automatikusan
                 xlWB.Close(false, Type.Missing, Type.Missing);
                 xlApp.Quit();
                 xlWB = null;
                 xlApp = null;
             }
         }
+        private void CreateTable()
+        {
+            string[] headers = new string[] 
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)",
+                ""
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                xlSheet.Cells[1,i+1] = headers[i];
+            }
+            object[,] values = new object[Flats.Count, headers.Length];
+            
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+        
 
     }
 }
