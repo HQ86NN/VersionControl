d26678cc6a4ef90e40687b2780ac1341eb1fa65d
Péter Gömöry <mozsunevol@gmail.com>
2020-11-13 16:49:50 +0100

    classok létrehozása

---
+++ Week7/Week7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7.Entities
+{
+    class DeathProbability
+    {
+        public int Age { get; set; }
+        public Gender gender { get; set; }
+        public double DProbability { get; set; }
+    }
+}
